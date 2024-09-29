e2704459919aef4a8fb9157de8df420b26efb34e
LaibaZehra <lz07603@st.habib.edu.pk>
2024-09-29 19:02:02 +0500

    slogan jsx and css

---
+++ IndieBazaar/src/components/slogan.jsx
@@ -0,0 +1,17 @@
+
+function Slogan() {
+    console.log("slogan function")
+    return (
+      <div className="promo-section">
+        <div className="promo-text">
+          <h1>Make a mark with your product</h1>
+        </div>
+        <div className="promo-image">
+          {/* Assuming an image of people shaking hands */}
+          {/* <img src="handshake.png" alt="Shaking hands" /> */}
+        </div>
+      </div>
+    );
+  }
+  
+  export default Slogan;
